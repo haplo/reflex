f898e3091aca0f385dd30235b464f285656940ca
Fidel Ramos <contact.gyldd@8shield.net>
2026-02-17 21:27:48 +0000

    Remove Flex-inherited Google Analytics config

--- example/publishconf.py
+++ example/publishconf.py
@@ -17,18 +17,3 @@ USE_LESS = False
 SHYNET_URL = (
     "https://shynet.fidelramos.net/ingress/9574b461-0b4e-4e54-adfc-1d49735686f1"
 )
-
-# GOOGLE_ANALYTICS = "UA-55543164-6"
-
-# GOOGLE_ADSENSE = {
-#     "ca_id": "ca-pub-6625957038449899",
-#     "page_level_ads": True,
-#     "ads": {
-#         "aside": "8752710348",
-#         "main_menu": "",
-#         "index_top": "",
-#         "index_bottom": "1124188687",
-#         "article_top": "",
-#         "article_bottom": "4843941849",
-#     },
-# }
